a03c8c39f508f23114f7a3be6d677ce57ca5781b
SHsuperCM <shaharcoolmen@gmail.Com>
2018-09-17 06:39:49 +0300

    Main Class

---
+++ src/main/java/shcm/SHsuperCM/forge/shcmbackupreborn/SHCMBackupReborn.java
@@ -0,0 +1,8 @@
+package shcm.SHsuperCM.forge.shcmbackupreborn;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = SHCMBackupReborn.MODID)
+public class SHCMBackupReborn {
+    public static final String MODID = "shcmbackupreborn";
+}
