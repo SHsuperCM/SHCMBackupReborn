ed3732683691560c34bc0c3c0972278545c5cea9
SHsuperCM <shaharcoolmen@gmail.Com>
2018-10-01 13:53:39 +0300

    Added Custom GUI support for OpenTerrainGenerator
    Changed how the button gets injected to the gui in order
    to support mods that change the whole gui instead of
    injecting things for some stupid reason...

--- src/main/java/SHCM/SHsuperCM/forge/shcmbackupreborn/client/ClientProxy.java
+++ src/main/java/SHCM/SHsuperCM/forge/shcmbackupreborn/client/ClientProxy.java
@@ -1,6 +1,17 @@
 package SHCM.SHsuperCM.forge.shcmbackupreborn.client;
 
+import SHCM.SHsuperCM.forge.shcmbackupreborn.client.gui.GuiWorldSelectionInjector;
 import SHCM.SHsuperCM.forge.shcmbackupreborn.common.CommonProxy;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void init(FMLPostInitializationEvent event) {
+        super.init(event);
+        GuiWorldSelectionInjector.guiInjector = GuiWorldSelectionInjector.Injectors.getSuggestedGuiInjector();
+    }
 }
--- src/main/java/SHCM/SHsuperCM/forge/shcmbackupreborn/client/gui/GuiWorldSelectionInjector.java
+++ src/main/java/SHCM/SHsuperCM/forge/shcmbackupreborn/client/gui/GuiWorldSelectionInjector.java
@@ -1,11 +1,11 @@
 package SHCM.SHsuperCM.forge.shcmbackupreborn.client.gui;
 
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiListWorldSelection;
-import net.minecraft.client.gui.GuiListWorldSelectionEntry;
-import net.minecraft.client.gui.GuiWorldSelection;
+import SHCM.SHsuperCM.forge.shcmbackupreborn.SHCMBackupReborn;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.world.storage.WorldSummary;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
@@ -13,35 +13,124 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static net.minecraftforge.fml.relauncher.ReflectionHelper.findField;
 
 @SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class GuiWorldSelectionInjector {
     private static GuiButton button;
 
+    public static Injectors.IGuiInjector guiInjector;
+
     @SubscribeEvent
-    public static void inject(GuiScreenEvent.InitGuiEvent.Post event) {
-        if(event.getGui() instanceof GuiWorldSelection) {
-            event.getButtonList().add(button = new GuiButton(97001,5,5,50,20, I18n.format("gui.shcmbackupreborn.backups")));
+    public static void inject(GuiScreenEvent.InitGuiEvent.Post event) throws ClassNotFoundException {
+        button = null;
+        guiInjector.inject(event);
+    }
+    @SubscribeEvent
+    public static void render(GuiScreenEvent.DrawScreenEvent.Pre event) throws IllegalAccessException, ClassNotFoundException, InvocationTargetException {
+        guiInjector.render(event);
+    }
+    @SubscribeEvent
+    public static void click(GuiScreenEvent.ActionPerformedEvent.Pre event) throws IllegalAccessException, InvocationTargetException {
+        guiInjector.click(event);
+    }
+
+
+    public static class Injectors {
+        public static IGuiInjector getSuggestedGuiInjector() {
+            if(Loader.isModLoaded("openterraingenerator")) {
+                return new Injectors.InjectorOTG();
+            }
+
+            return new Injectors.InjectorVanilla();
+        }
+
+        public interface IGuiInjector {
+            void inject(GuiScreenEvent.InitGuiEvent.Post event);
+            void render(GuiScreenEvent.DrawScreenEvent.Pre event) throws IllegalAccessException, InvocationTargetException;
+            void click(GuiScreenEvent.ActionPerformedEvent.Pre event) throws IllegalAccessException, InvocationTargetException;
+        }
+
+        public static class InjectorVanilla implements IGuiInjector {
+            private Field GuiWorldSelection_selectionList;
+            private Field GuiListWorldSelectionEntry_worldSummary;
+
+            InjectorVanilla() {
+                try {
+                    GuiWorldSelection_selectionList = findField(GuiWorldSelection.class, "selectionList", "field_184866_u");
+                    GuiListWorldSelectionEntry_worldSummary = findField(GuiListWorldSelectionEntry.class, "worldSummary", "field_186786_g");
+                } catch (ReflectionHelper.UnableToFindFieldException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void inject(GuiScreenEvent.InitGuiEvent.Post event) {
+                if (event.getGui() instanceof GuiWorldSelection)
+                    event.getButtonList().add(button = new GuiButton(97001, 5, 5, 50, 20, I18n.format("gui.shcmbackupreborn.backups")));
+            }
+
+            @Override
+            public void render(GuiScreenEvent.DrawScreenEvent.Pre event) throws IllegalAccessException {
+                if(event.getGui() instanceof GuiWorldSelection)
+                    button.enabled = ((GuiListWorldSelection)GuiWorldSelection_selectionList.get(event.getGui())).getSelectedWorld() != null;
+            }
+
+            @Override
+            public void click(GuiScreenEvent.ActionPerformedEvent.Pre event) throws IllegalAccessException {
+                if(event.getGui() instanceof GuiWorldSelection && event.getButton().id == button.id) {
+                    GuiListWorldSelection list = (GuiListWorldSelection) GuiWorldSelection_selectionList.get(event.getGui());
+                    if(list.getSelectedWorld() != null) {
+                        WorldSummary worldSummary = (WorldSummary)GuiListWorldSelectionEntry_worldSummary.get(list.getSelectedWorld());
+                        SHCMBackupReborn.logger.info("backups gui open " + worldSummary.getFileName());
+                    }
+                }
+            }
+        }
+
+        public static class InjectorOTG implements IGuiInjector {
+            private Class OTGGuiWorldSelection;
+            private Field OTGGuiWorldSelection_selectionList;
+            private Method OTGGuiListWorldSelection_getSelectedWorld;
+            private Field OTGGuiListWorldSelectionEntry_worldSummary;
+
+            InjectorOTG() {
+                try {
+                    OTGGuiWorldSelection = Class.forName("com.pg85.otg.forge.gui.OTGGuiWorldSelection");
+                    (OTGGuiWorldSelection_selectionList = OTGGuiWorldSelection.getDeclaredField("selectionList")).setAccessible(true);
+                    OTGGuiListWorldSelection_getSelectedWorld = Class.forName("com.pg85.otg.forge.gui.OTGGuiListWorldSelection").getDeclaredMethod("getSelectedWorld");
+                    (OTGGuiListWorldSelectionEntry_worldSummary = Class.forName("com.pg85.otg.forge.gui.OTGGuiListWorldSelectionEntry").getDeclaredField("worldSummary")).setAccessible(true);
+                } catch (ClassNotFoundException|NoSuchFieldException|NoSuchMethodException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void inject(GuiScreenEvent.InitGuiEvent.Post event) {
+                if(OTGGuiWorldSelection.equals(event.getGui().getClass()))
+                    event.getButtonList().add(button = new GuiButton(97001, 5, 5, 50, 20, I18n.format("gui.shcmbackupreborn.backups")));
+            }
+
+            @Override
+            public void render(GuiScreenEvent.DrawScreenEvent.Pre event) throws IllegalAccessException, InvocationTargetException {
+                if(OTGGuiWorldSelection.equals(event.getGui().getClass()))
+                    button.enabled = OTGGuiListWorldSelection_getSelectedWorld.invoke(OTGGuiWorldSelection_selectionList.get(event.getGui())) != null;
+            }
+
+            @Override
+            public void click(GuiScreenEvent.ActionPerformedEvent.Pre event) throws IllegalAccessException, InvocationTargetException {
+                if(OTGGuiWorldSelection.equals(event.getGui().getClass()) && event.getButton().id == button.id) {
+                    Object selectedWorld = OTGGuiListWorldSelection_getSelectedWorld.invoke(OTGGuiWorldSelection_selectionList.get(event.getGui()));
+                    if(selectedWorld != null) {
+                        WorldSummary worldSummary = (WorldSummary) OTGGuiListWorldSelectionEntry_worldSummary.get(selectedWorld);
+                        SHCMBackupReborn.logger.info("backups gui open " + worldSummary.getFileName());
+                    }
+                }
+            }
         }
     }
-
-    @SubscribeEvent
-    public static void render(GuiScreenEvent.DrawScreenEvent.Pre event) throws IllegalAccessException {
-        if(event.getGui() instanceof GuiWorldSelection)
-            button.enabled = ((GuiListWorldSelection)GuiWorldSelection_selectionList.get(event.getGui())).getSelectedWorld() != null;
-    }
-
-    @SubscribeEvent
-    public static void click(GuiScreenEvent.ActionPerformedEvent.Pre event) throws IllegalAccessException {
-        if(event.getGui() instanceof GuiWorldSelection && event.getButton().id == button.id) {
-            GuiListWorldSelection list = (GuiListWorldSelection) GuiWorldSelection_selectionList.get(event.getGui());
-            if(list.getSelectedWorld() != null)
-                ;//Backups GUI for world
-        }
-    }
-
-
-    private static Field GuiWorldSelection_selectionList = ReflectionHelper.findField(GuiWorldSelection.class,"selectionList", "field_184866_u");
-    private static Field GuiListWorldSelectionEntry_worldSummary = ReflectionHelper.findField(GuiListWorldSelectionEntry.class,"worldSummary", "field_186786_g");
 }
